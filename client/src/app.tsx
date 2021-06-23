76d2bf9debdac904f82a778d50d1556200564b2e
Justin Kook <justinkook@gmail.com>
2021-06-23 06:14:35 -0400

    WIP add landing page (need to fix css)

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import CreatePage from "./pages/CreatePage";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/:path(|create-my-nft)">
+        <Route path="/create-page">
           <CreatePage {...createPageData} />
+        </Route>
+        <Route path="/:path(|landing-page)">
+          <LandingPage {...landingPageData} />
         </Route>
       </Switch>
     </Router>
@@ -80,4 +84,39 @@ const createPageData = {
   createItem: "Create item",
   sliderProps: slider2Data,
   slider2Props: slider3Data,
+};
+
+const landingPageData = {
+  maskGroup: "/img/mask-group@2x.svg",
+  overlapGroup6: "/img/rectangle-760@2x.png",
+  rectangle763: "/img/rectangle-763-1@2x.png",
+  overlapGroup11: "/img/rectangle-762@2x.png",
+  overlapGroup4: "/img/rectangle-768@2x.png",
+  rectangle769: "/img/rectangle-769-1@2x.png",
+  overlapGroup10: "/img/mask-group-1@2x.svg",
+  rectangle711: "/img/rectangle-711-1@2x.png",
+  rectangle765: "/img/rectangle-765@2x.png",
+  rectangle770: "/img/rectangle-770-1@2x.png",
+  overlapGroup8: "/img/rectangle-764@2x.png",
+  overlapGroup3: "/img/rectangle-767@2x.png",
+  rectangle719: "/img/rectangle-719-1@2x.png",
+  group987: "/img/mask-group-3@2x.svg",
+  rectangle766: "/img/rectangle-766@2x.png",
+  maskGroup2: "/img/mask-group-6@2x.svg",
+  image11: "/img/image-11@2x.png",
+  explore: "EXPLORE",
+  community: "COMMUNITY",
+  learn: "LEARN",
+  line1: "/img/line-1@2x.svg",
+  create: "Create",
+  connectWallet: "Connect Wallet",
+  rectangle721: "/img/rectangle-721@1x.svg",
+  rectangle753: "/img/rectangle-753@1x.svg",
+  motorcycles: "Make the next big thing today",
+  getStartedButton: "/img/get-started-button@2x.svg",
+  getStartedButton2: "/img/get-started-button-1@2x.svg",
+  getStartedButton3: "/img/get-started-button-2@2x.svg",
+  line22: "/img/line-22@1x.svg",
+  text1: "Or scroll to explore",
+  rectangle722: "/img/rectangle-722@2x.svg",
 };
