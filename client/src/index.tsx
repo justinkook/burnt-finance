8d347ea43ddac88f713a9ae457891eb300b7d855
Justin Kook <justinkook@gmail.com>
2021-06-23 02:28:36 -0400

    Register serviceWorker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorker from 'serviceWorker';
 
 import "styles/index.scss";
 
 import App from "./app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.register();
